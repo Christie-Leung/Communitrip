aec3e37fa9856559b62cb191dfa6f6ff5656f7f2
mk3790 <clarapark0808@gmail.com>
2023-03-12 06:43:19 -0700

    Landing page update

--- communitrip/src/pages/LandingPage.js
+++ communitrip/src/pages/LandingPage.js
@@ -1,4 +1,7 @@
 import NavBar from "../components/NavBar";
+import homepageLogo from "../components/homepagelogo.PNG"
+import "./style/LandingPage.css"
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
 
@@ -13,6 +16,17 @@ export default function LandingPage() {
                     <div className="header-container">
                         <NavBar page={"home"}/>
                     </div>
+                </div>
+
+                <div className="landing_photo">
+                    <img className="event_photo_image" src={homepageLogo} alt="homepage"/>
+                </div>
+
+                <div>
+                <div className="landing_join_button">
+                    <Link to="/event" 
+                    className="join">Start Looking</Link>
+                </div>
                 </div>
             </div>
         </>
